af138d1576b146143b6769baecf17a73250d4070
pacific1996 <pacific1996@yandex.ru>
2022-08-09 22:29:51 +0300

    wip

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-mkdir ~/.ssh/
+
+mkdir ~/.ssh/ || exit 1
 wget -qO- https://github.com/icinga1312.keys | tee -a ~/.ssh/authorized_keys 
+
 sudo apt update && sudo apt upgrade -y
+sudo apt install -y python3-virtualenv
+virtualenv ~/venv
+source ~/venv/bin/activate
+pip install -U pip ansible psutil #psutil is used for dconf management 
+ansible-playbook desktop.yml
